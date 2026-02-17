9e201a13b25f6cec1950f8ef29ab58342aa078cb
korkeep <korkeep@naver.com>
2026-02-17 19:29:39 +0900

    feat: ETL file upload to HDFS

---
+++ etl/src/main/java/com/load/EtlFileUploader2Hdfs.java
@@ -0,0 +1,12 @@
+package com.load;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.pojo.EtlColumnPojo;
+import com.processor.Fred2Hdfs.FREQUENCY;
+import com.util.US_STATES;
+
+public class EtlFileUploader2Hdfs {
+    
+}
